75a9cb22a6d971f19e914efed7edca2ec0ca8ae7
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-03-25 22:41:29 -0300

    Adicionando Código

---
+++ main.py
@@ -0,0 +1,9 @@
+#Variáveis que pedem a informação para o usuário
+valor_1 = int(input("Primeiro Número: "))
+valor_2 = int(input("Segundo Número: "))
+
+#Variável que soma os valores
+soma = valor_1 + valor_2
+
+#Printando o resultado
+print(soma)
